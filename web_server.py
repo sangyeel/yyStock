1793ae82306072de77e97af85362ca20780fc1db
Sang-Yeel.Ji <sangyeel87@naver.com>
2025-10-18 21:43:07 +0900

    Add 3day stock data visualization feature

--- web_server.py
+++ web_server.py
@@ -1,6 +1,7 @@
 # Flask 라이브러리가 설치되어 있지 않다면, 터미널에서 pip install Flask 를 실행해주세요.
 # pykrx 라이브러리가 설치되어 있지 않다면, 터미널에서 pip install pykrx 를 실행해주세요.
 from flask import Flask, request, render_template
+import pandas as pd
 from pykrx import stock
 from datetime import datetime, timedelta
 import time
@@ -107,6 +108,35 @@ def show_table():
     all_data, working_days = StockDataCache().get_data()
     data_dict = all_data.get(market, {})
 
+    # 3일 연속 거래량 증가 종목 필터링 로직
+    filtered_tickers = None
+    if sort_by == 'volume_increase_3_days':
+        filtered_tickers = []
+        if len(working_days) >= 4:
+            # working_days는 최신순으로 정렬되어 있음: ['오늘', '어제', '그제', ...]
+            df_d0 = data_dict.get(working_days[0])
+            df_d1 = data_dict.get(working_days[1])
+            df_d2 = data_dict.get(working_days[2])
+            df_d3 = data_dict.get(working_days[3])
+
+            # 4일치 데이터가 모두 있고, '거래량' 컬럼이 있는지 확인
+            if all(df is not None and not df.empty and '거래량' in df.columns for df in [df_d0, df_d1, df_d2, df_d3]):
+                common_tickers = df_d0.index.intersection(df_d1.index).intersection(df_d2.index).intersection(df_d3.index)
+                
+                for ticker in common_tickers:
+                    try:
+                        vol0 = df_d0.loc[ticker, '거래량']
+                        vol1 = df_d1.loc[ticker, '거래량']
+                        vol2 = df_d2.loc[ticker, '거래량']
+                        vol3 = df_d3.loc[ticker, '거래량']
+
+                        # 0으로 나누는 것을 방지하고, 3일 연속 30% 이상 증가했는지 확인
+                        if vol1 > 0 and vol2 > 0 and vol3 > 0:
+                            if (vol0 / vol1 > 1.3) and (vol1 / vol2 > 1.3) and (vol2 / vol3 > 1.3):
+                                filtered_tickers.append(ticker)
+                    except (KeyError, ZeroDivisionError):
+                        continue # 티커가 없거나 거래량이 0인 경우 건너뜀
+
     daily_data = []
     for day in working_days:
         df = data_dict.get(day)
@@ -114,25 +144,34 @@ def show_table():
         day_info = {'date': day_formatted}
 
         if df is not None and not df.empty:
-            if sort_by == 'turnover_desc' and '일일회전율' in df.columns:
-                df_sorted = df.sort_values(by='일일회전율', ascending=False)
-            elif sort_by == 'turnover_asc' and '일일회전율' in df.columns:
-                df_sorted = df.sort_values(by='일일회전율', ascending=True)
-            elif sort_by == 'rate_desc' and '등락률' in df.columns:
-                df_sorted = df.sort_values(by='등락률', ascending=False)
-            elif sort_by == 'rate_asc' and '등락률' in df.columns:
-                df_sorted = df.sort_values(by='등락률', ascending=True)
-            elif sort_by == 'volume_asc' and '거래량' in df.columns:
-                df_sorted = df.sort_values(by='거래량', ascending=True)
-            elif sort_by == 'volume_desc' and '거래량' in df.columns:
-                df_sorted = df.sort_values(by='거래량', ascending=False)
-            else: # 기본 정렬
-                df_sorted = df.sort_values(by='거래량', ascending=False)
+            
+            df_display_source = df
+            # 필터링된 종목이 있으면 해당 종목만 사용
+            if filtered_tickers is not None:
+                df_display_source = df[df.index.isin(filtered_tickers)]
+
+            if df_display_source.empty:
+                day_info['data'] = None
+                day_info['headers'] = []
+                daily_data.append(day_info)
+                continue
+
+            # 정렬 로직
+            if sort_by == 'turnover_desc' and '일일회전율' in df_display_source.columns:
+                df_sorted = df_display_source.sort_values(by='일일회전율', ascending=False)
+            elif sort_by == 'turnover_asc' and '일일회전율' in df_display_source.columns:
+                df_sorted = df_display_source.sort_values(by='일일회전율', ascending=True)
+            elif sort_by == 'rate_desc' and '등락률' in df_display_source.columns:
+                df_sorted = df_display_source.sort_values(by='등락률', ascending=False)
+            elif sort_by == 'rate_asc' and '등락률' in df_display_source.columns:
+                df_sorted = df_display_source.sort_values(by='등락률', ascending=True)
+            elif sort_by == 'volume_asc' and '거래량' in df_display_source.columns:
+                df_sorted = df_display_source.sort_values(by='거래량', ascending=True)
+            else: # 'volume_desc' 또는 'volume_increase_3_days' (기본 정렬)
+                df_sorted = df_display_source.sort_values(by='거래량', ascending=False)
 
             columns_to_show = ['종목명', '거래량', '일일회전율', '등락률', '종가']
-
-            # Ensure we only select columns that actually exist in the DataFrame
-            existing_columns_to_show = [col for col in columns_to_show if col in df.columns]
+            existing_columns_to_show = [col for col in columns_to_show if col in df_sorted.columns]
             df_display = df_sorted[existing_columns_to_show].head(20)
 
             day_info['headers'] = existing_columns_to_show
